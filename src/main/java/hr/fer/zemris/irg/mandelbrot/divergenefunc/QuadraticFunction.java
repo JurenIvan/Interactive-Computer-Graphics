935bf9507fc22c661f2dcd763a1f55b7e1361302
jurenivan <juren.ivan@gmail.com>
2020-05-14 11:10:49 +0200

    major perfomance improvement

--- src/main/java/hr/fer/zemris/irg/mandelbrot/divergenefunc/QuadraticFunction.java
+++ src/main/java/hr/fer/zemris/irg/mandelbrot/divergenefunc/QuadraticFunction.java
@@ -12,12 +12,29 @@ public class QuadraticFunction implements Function<Complex, Short> {
         this.maxIterations = maxIterations;
     }
 
+//    @Override
+//    public Short apply(Complex complex) {
+//        Complex c = Complex.ZERO;
+//        for (short i = 0; i < maxIterations; i++) {
+//            c = c.multiply(c).add(complex);
+//            if (c.module() > 2) return i;
+//        }
+//        return -1;
+//    }
+
     @Override
     public Short apply(Complex complex) {
-        Complex c = Complex.ZERO;
-        for (short i = 0; i < maxIterations; i++) {
-            c = c.multiply(c).add(complex);
-            if (c.module() > 2) return i;
+        double reC = complex.getRe();
+        double imC = complex.getIm();
+
+        for (short i = 1; i <= maxIterations; i++) {
+
+            double nextRe = reC * reC - imC * imC + complex.getRe();
+            double nextIm = 2 * reC * imC + complex.getIm();
+            reC = nextRe;
+            imC = nextIm;
+
+            if (reC * reC + imC * imC > 4) return i;
         }
         return -1;
     }
